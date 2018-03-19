bbbdf68f5a1eb87a9dbbc50842be6990a7ccc8cb
Sofia Khizhnia <sofabbk@gmail.com>
2018-03-19 14:17:27 +0200

    Task#21269: add function for top navigation

--- main_b.js
+++ main_b.js
@@ -2,10 +2,28 @@
 function ready() {
     var next = document.querySelector('.selector-next');
     var prev = document.querySelector('.selector-prev');
-    document.querySelector('.selector-find').onclick = function () {
+
+    var top = document.querySelector('.nav-top');
+    var bottom = document.querySelector('.nav-bottom');
+    var left = document.querySelector('.nav-left');
+    var right = document.querySelector('.nav-right');
+
+    function ff(){
         var findSelector = document.querySelector('.selector').value;
-        var findBlock = document.getElementsByClassName(findSelector);
-        if (findBlock.length > 1) { next.disabled=false; prev.disabled=false; }
+        return  document.getElementsByClassName(findSelector);
+    }
+
+    document.querySelector('.selector-find').onclick = function () {
+
+        var findBlock = ff();
+
+        if (findBlock.length > 1) {
+            next.disabled=false;
+            prev.disabled=false;
+            next.addEventListener("click", nextf);
+            prev.addEventListener("click", prevf);
+        }
+        clearSelect ();
         select (findBlock[0]);
     };
 
@@ -13,29 +31,44 @@ function ready() {
         el.style="outline:solid red 5px;background-color:lightblue;";
         el.setAttribute('data_state','_active');
     }
-
-
-    next.onclick = function (el) {
-    };
-
-
-    function prev () {
-        console.log('prev');
+    function clearSelect () {
+        var findBlock = ff();
+        for (i=0; i<findBlock.length; i++){
+            findBlock[i].removeAttribute('style');
+            findBlock[i].removeAttribute('data_state');
+        }
     }
 
+    var count = 0;
+    function nextf () {
+        var findBlock = ff();
+        if (count < findBlock.length-1){
+            count++;
+            clearSelect ();
+            select(findBlock[count]);
+        }
+    }
+    function prevf () {
+        var findBlock = ff();
+        if (count > 0){
+            count--;
+            clearSelect ();
+            select(findBlock[count]);
+        }
+    }
 
 //tree navigation
 
-    function parent () {
+    function top () {
         console.log('parent');
     }
-    function firstChild () {
+    function bottom () {
         console.log('firstChild');
     }
-    function prevSim () {
+    function left () {
         console.log('prevSim');
     }
-    function nextSim () {
+    function right () {
         console.log('nextSim');
     }
 }
