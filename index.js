6bc0214c63efe930f03f9e22b97d5f63c6c4ea36
Sofia Khizhnia <sofabbk@gmail.com>
2018-03-19 18:27:17 +0200

    Task#12200: up

--- index.js
+++ index.js
@@ -7,6 +7,30 @@ var colors = [
     'pink'
 ];
 
-window.onload = function () {
+document.onload = function () {
     //ваш код будет здесь
-};
+    window.onclick = function(event) {
+        var target = event.target;
+        console.log(target);
+        alert('1111');
+        // цикл двигается вверх от target к родителям до table
+        // while (target != this) {
+        //     console.log(target);
+        //     if (target.tagName == 'TD') {
+        //         // нашли элемент, который нас интересует!
+        //         highlight(target);
+        //         return;
+        //     } else if (target.tagName == 'TD') {
+        //         // нашли элемент, который нас интересует!
+        //         highlight(target);
+        //         return;
+        //     } else if (target.tagName == 'TD') {
+        //         // нашли элемент, который нас интересует!
+        //         highlight(target);
+        //     return;
+        //     }
+        //     target = target.parentNode;
+        // }
+
+    }
+}
