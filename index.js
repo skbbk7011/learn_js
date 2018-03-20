4a507c301f865c7170ce449e3258fd0db101ac76
Sofia Khizhnia <sofabbk@gmail.com>
2018-03-20 15:50:05 +0200

    Task#12200: ex2 up

--- index.js
+++ index.js
@@ -8,57 +8,34 @@ var colors = [
 ];
 
 window.onload = function () {
-    //var body = document.getElementsByTagName('body')[0];
-    // body.onclick = function(event) {
-    //     var target = event.target;
-    //     var randomizeColor = rand(colors);
-    //     while (target != this) {
-    //         if (target.className == 'el1') {
-    //             recolor(target, randomizeColor);
-    //             valid();
-    //             return;
-    //         } else if (target.className == 'el2') {
-    //             recolor(target, randomizeColor);
-    //             valid();
-    //             return;
-    //         } else if (target.className == 'el3') {
-    //             recolor(target, randomizeColor);
-    //             valid();
-    //             return;
-    //         }
-    //         target = target.parentNode;
-    //
-    //     }
-    //
-    // }
-
-
     var count = prompt()*1;
     generate(count);
-
     var css = generateCss(count),
         head = document.head || document.getElementsByTagName('head')[0],
         style = document.createElement('style');
-
     style.type = 'text/css';
     if (style.styleSheet){
         style.styleSheet.cssText = css;
     } else {
         style.appendChild(document.createTextNode(css));
     }
-
     head.appendChild(style);
 
     function generateCss(count){
         var size = 50;
         var css = '';
+        var relative = 'position:relative;';
+        var absolute = 'position:absolute; top:10px; left:10;';
         for (i=count; i > -1; i--){
-            css += '.el' + i + '{width:' + size + 'px; height:' + size +'px;}';
+            if(i==1){
+                css += '.el' + i + '{' + relative + ' width:' + size + 'px; height:' + size +'px;}';
+            }else{
+                css += '.el' + i + '{' + absolute + ' width:' + size + 'px; height:' + size +'px;}';
+            }
             size += 20;
         }
         return css;
     }
-
     function generate (count) {
         for(i=1; i < count+1; i++){
             var color = rand(colors);
@@ -70,38 +47,38 @@ window.onload = function () {
             }
             document.querySelector('.el' + i).style.backgroundColor = color;
         }
+        randRecolor();
     }
-
     function rand (mass){
         var rand = Math.floor(Math.random() * mass.length);
         return mass[rand];
     }
-
     function recolor (elem, color){
         elem.style.backgroundColor = color;
     }
-
-    function valid (){
-
+    function randRecolor(){
+        var color = rand(colors);
+        var arrBlock = [];
+        var elArr = document.querySelectorAll('div[class^="el"]');
+        for (i=0; i< elArr.length; i++){
+            arrBlock.push(elArr[i]);
+        }
+        recolor(rand(arrBlock), color);
+        valid();
+    };
+    function valid(){
         var collectionEl = document.querySelectorAll('div[class^=\'el\']');
         var arrColors = [];
+
         for(i=0; i<collectionEl.length;i++){
             arrColors.push(collectionEl[i].style.backgroundColor);
         }
 
-            function foo(a) {
-                return !a.some(function(b) {
-                    return b !== a[0]
-                })
-            };
-
-        if (foo(arrColors) == true){
-            console.log('done');
-        }
+        console.log(foo(arrColors));
     }
-
-
-
-
-
+    function foo(a) {
+        return !a.some(function(b) {
+            return b !== a[0]
+        })
+    }
 };
