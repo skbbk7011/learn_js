be34e35c46a4aef7d232d5b2a5e79a95b362ae64
Sofia Khizhnia <sofabbk@gmail.com>
2018-03-23 12:57:02 +0200

    Task#12484: done

--- index.js
+++ index.js
@@ -2,12 +2,9 @@ function send() {
     var graph = document.querySelectorAll('.graphbar');
     var price = document.querySelector('.value');
     var socket = new WebSocket("ws://127.0.0.1:3000/ws");
-
-
     socket.onopen = function() {
         console.log("Соединение установлено.");
     };
-
     socket.onclose = function(event) {
         if (event.wasClean) {
             alert('Соединение закрыто чисто');
@@ -18,46 +15,46 @@ function send() {
     };
 
     var i = 0;
-    var ticketNum = '';
+    var dataReq = {};
     socket.onmessage = function(event) {
         var json = JSON.parse(event.data);
-
-
         i++;
         if (i > graph.length-1){
             i = 0;
         }
-
         if (json.price > 200){
             price.style.color = 'green';
         } else {
             price.style.color = 'red';
         }
         price.textContent = json.price.toFixed(1);
-
         graph[i].style.height = json.price+'px';
-
-        ticketNum = json.ticket;
+        dataReq = event.data;
     };
     socket.onerror = function(error) {
         alert("Ошибка " + error.message);
     };
 
+    var amount = 0;
 
     document.querySelector('button').addEventListener('click', function () {
-
-        //127.0.0.1:3000/sell
-        // script = document.createElement("script");
-        // script.type = "text/javascript";
-        // script.src = "http://127.0.0.1:3000/sell?data="+ticketNum+"&format=json&callback=toJsonContainer";
-        //
-        // document.getElementsByTagName('head')[0].appendChild(script);
-        //
-        // toJsonContainer = function(data){
-        //     console.log(data);
-        // };
-
-
+        var xhttp2 = new XMLHttpRequest();
+        xhttp2.onreadystatechange = function() {
+            var jsonAnswer = JSON.parse(this.response);
+            var error = 'sell was ' + jsonAnswer.success+ ', becouse ' + jsonAnswer.reason;
+            if (this.status == 200 && this.readyState == 4){
+                if (aa.success == true){
+                    amount += jsonAnswer.lag;
+                    document.querySelector('.sellAmount').textContent = amount;
+                }
+            } else if ((this.status == 400 && this.readyState == 4) || (this.status == 404 && this.readyState == 4)){
+                alert(error);
+            } else if (this.status == 403 && this.readyState == 4){
+                alert(error + ' = ' + jsonAnswer.sellLimit);
+            }
+        };
+        xhttp2.open("POST", "http://127.0.0.1:3000/sell", true);
+        xhttp2.send(dataReq);
 
     })
 
