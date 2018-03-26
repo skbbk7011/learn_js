9374c1615bf12ecd68ce8a717d30530231871577
Sofia Khizhnia <sofabbk@gmail.com>
2018-03-26 15:40:36 +0300

    Task#11925 fly ball

--- common.js
+++ common.js
@@ -1,105 +1,38 @@
-//svetofor
-/*** Сервисный код. Пока в нем можно не разбираться ***/
-document.addEventListener('click', function (evt) {
-    if (evt.target.nodeName.toLowerCase() === 'button') {
-        processButtonClick(evt.target);
-    }
-});
+function ready() {
 
-function closest(el, selector) {
-    var first = selector.charAt(0);
-    // Traverse the DOM tree using parentNode
-    for (; el && el !== document && el.nodeType === 1; el = el.parentNode) {
-        // Selector is a class
-        if (first === '.' && el.classList.contains(selector.substr(1))) {
-            return el;
+    var canvas = document.getElementById('borders');
+    var canvasW = document.getElementById('borders').clientWidth;
+    var canvasH = document.getElementById('borders').clientHeight;
+    var elem = document.getElementById('flyer');
+    var stepLeft = elem.getBoundingClientRect().left;
+    var stepDown = elem.getBoundingClientRect().top;
+    var dx = 2;
+    var dy = 2;
+
+    function draw (){
+        stepLeft += dx;
+        stepDown += dy;
+        if (stepLeft+elem.clientWidth > canvasW){
+             dx = -2;
+        } else if (stepLeft < 0){
+            dx = 2;
         }
-        // Selector is a tagName
-        if (el.tagName.toLowerCase() === selector) {
-            return el;
+        if(stepDown+elem.clientHeight > canvasH){
+            dy = -2;
+        } else if (stepDown < 0){
+            dy = 2;
         }
+
+        elem.style.left = stepLeft+'px';
+        elem.style.top = stepDown+'px';
+
+
+        requestAnimationFrame(draw);
     }
-    return null;
-};
-function processButtonClick(el) {
-    var closestElem = closest(el, '.inputzone');
-    var colorBlock = closestElem.getElementsByClassName('light');
-    colorBlock[0].style.backgroundColor = 'blue';
-    closestElem.classList.add("done");
-    inspectLamp(el);
-}
-function inspectLamp(el) {
-    var newChild = '<p style="color:green">Done</p>'
-    var parentOb = closest(el, '.border');
-    var itemCollectionLenght = parentOb.getElementsByClassName('inputzone').length;
-    var itemCollectionDone = parentOb.getElementsByClassName('done').length;
-    if (itemCollectionDone == itemCollectionLenght) {
-        parentOb.insertAdjacentHTML('beforeend', newChild);
-    }
+    draw ();
+
 }
 
-function task11925_1(){
-    function printResultToDiv(value, target) {
-        document.querySelector('#' + target).textContent = value;
-    }
-
-    function printArrayToDiv(array, target) {
-        printResultToDiv('[' + array.join(', ') + ']', target);
-    }
-
-    function cleanArray (arr) {
-        var result = [];
-        for(i=0; i<arr.length-1; i++){
-            if(typeof(1) === typeof(arr[i])){
-                result.push(arr[i]);
-            }
-        }
-        return result;
-    }
-
-    function average (arr) {
-        var result = 1;
-        for (i=0;i<arr.length;i++){
-            result = result * arr[i];
-        }
-        result = Math.pow(result,1/arr.length);
-        return result;
-    }
-
-    var sourceArray = [1, "37", 45, "котик", undefined, null, "44", "22.3"];
-    printArrayToDiv(sourceArray, 'source');
-
-    var cleanedArray = cleanArray(sourceArray);
-    printArrayToDiv(cleanedArray, 'result');
-
-    printResultToDiv(average(cleanedArray), 'avg');
-
-}
-document.addEventListener('DOMContentLoaded', task11925_1);
 
 
-function task11925_2(){
-    function linearize (inArr) {
-        console.log(inArr);
-        var outArr = [];
-
-        for (f=0;f<inArr.length; f++){
-            for (j=0;j<inArr[f].length; j++){
-                outArr.push(inArr[f][j]);
-            }
-        }
-        print(inArr, outArr);
-    }
-
-    function print(defMass, linearize){
-        console.log(linearize);
-        console.log(defMass + ' --> ' + linearize);
-    }
-
-    linearize([[1],[2], [3, 4, 5]]);
-    linearize([[1, 2], [3], [4], [5]]);
-    linearize([[1, 2], [3,[[6],[7]]], [4], [5]]);
-
-
-}
-document.addEventListener('DOMContentLoaded', task11925_2);
+document.addEventListener('DOMContentLoaded', ready);
