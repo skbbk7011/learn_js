bd2b4d9f991ba6bce8432a7c5032794aa64ef4f6
Sofia Khizhnia <sofabbk@gmail.com>
2018-03-26 16:25:03 +0300

    create obj

--- common.js
+++ common.js
@@ -1,35 +1,72 @@
 function ready() {
 
-    var canvas = document.getElementById('borders');
-    var canvasW = document.getElementById('borders').clientWidth;
-    var canvasH = document.getElementById('borders').clientHeight;
-    var elem = document.getElementById('flyer');
-    var stepLeft = elem.getBoundingClientRect().left;
-    var stepDown = elem.getBoundingClientRect().top;
-    var dx = 2;
-    var dy = 2;
+     var count = prompt();
+     if (count >= 5){
+         alert('count < 5');
+         var count = prompt();
+     }
+    create(count);
+    function create(c) {
+        var color = [
+            red,
+            grey,
+            yellow,
+            green,
+            pink
+        ];
+        console.log(c);
+        //width: 40px;height: 40px;background-color: blue;border-radius: 20px;left: 40px;top: 200px;
 
-    function draw (){
-        stepLeft += dx;
-        stepDown += dy;
-        if (stepLeft+elem.clientWidth > canvasW){
-             dx = -2;
-        } else if (stepLeft < 0){
-            dx = 2;
-        }
-        if(stepDown+elem.clientHeight > canvasH){
-            dy = -2;
-        } else if (stepDown < 0){
-            dy = 2;
-        }
-
-        elem.style.left = stepLeft+'px';
-        elem.style.top = stepDown+'px';
-
-
-        requestAnimationFrame(draw);
     }
-    draw ();
+
+    var fly = function(el){
+        var canvas = document.getElementById('borders');
+        var canvasW = canvas.clientWidth;
+        var canvasH = canvas.clientHeight;
+
+        var dx = 2;
+        var dy = 2;
+
+
+        this.init = function (el) {
+            var elem = document.getElementById(el);
+            var stepLeft = elem.getBoundingClientRect().left;
+            var stepDown = elem.getBoundingClientRect().top;
+            draw();
+        };
+
+        this.create = function () {
+
+        };
+
+
+        function draw (){
+            stepLeft += dx;
+            stepDown += dy;
+            if (stepLeft+elem.clientWidth > canvasW){
+                dx = -2;
+            } else if (stepLeft < 0){
+                dx = 2;
+            }
+            if(stepDown+elem.clientHeight > canvasH){
+                dy = -2;
+            } else if (stepDown < 0){
+                dy = 2;
+            }
+
+            elem.style.left = stepLeft+'px';
+            elem.style.top = stepDown+'px';
+
+            requestAnimationFrame(draw);
+        }
+
+    };
+
+    var fly1 = new fly('flyer1');
+    fly1.init();
+
+    var fly2 = new fly('flyer2');
+    fly2.init();
 
 }
 
