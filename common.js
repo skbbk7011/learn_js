b0cbae01aef1fc67de8c445b170ee64493181971
Sofia Khizhnia <sofabbk@gmail.com>
2018-03-26 17:56:43 +0300

    Task#12200: many flyed balls

--- common.js
+++ common.js
@@ -1,39 +1,62 @@
 function ready() {
 
-     var count = prompt();
-     if (count >= 5){
-         alert('count < 5');
-         var count = prompt();
-     }
-    create(count);
+    var canvas = document.getElementById('borders');
+    var canvasW = canvas.clientWidth;
+    var canvasH = canvas.clientHeight;
+
+    function rand (mass){
+        var rand = Math.floor(Math.random() * mass.length);
+        return mass[rand];
+    }
+
+    function randomInteger(min, max) {
+        var rand = min + Math.random() * (max + 1 - min);
+        rand = Math.floor(rand);
+        return rand;
+    }
+
     function create(c) {
-        var color = [
-            red,
-            grey,
-            yellow,
-            green,
-            pink
+        var colors = [
+            'red',
+            'grey',
+            'yellow',
+            'green',
+            'pink'
         ];
-        console.log(c);
-        //width: 40px;height: 40px;background-color: blue;border-radius: 20px;left: 40px;top: 200px;
+
+        for (i=0;i<count;i++){
+
+            var el = document.createElement('div');
+            el.id = 'flyer'+i;
+            el.style = 'width: 40px;' +
+                'height: 40px;' +
+                'background-color: '+ rand(colors) +';' +
+                'border-radius: 20px;' +
+                'left: '+randomInteger(30, canvasW-30)+'px;' +
+                'top: '+randomInteger(30, canvasH-30)+'px;';
+            document.getElementById('borders').appendChild(el);
+            var nameVar = 'fly'+i;
+            var name = 'flyer'+i;
+            eval('var nameVar = new fly(name)') ;
+           // eval('nameVar.init()');
+
+        }
 
     }
 
     var fly = function(el){
-        var canvas = document.getElementById('borders');
-        var canvasW = canvas.clientWidth;
-        var canvasH = canvas.clientHeight;
+
 
         var dx = 2;
         var dy = 2;
 
 
-        this.init = function (el) {
+       // this.init = function (el) {
             var elem = document.getElementById(el);
             var stepLeft = elem.getBoundingClientRect().left;
             var stepDown = elem.getBoundingClientRect().top;
             draw();
-        };
+      //  };
 
         this.create = function () {
 
@@ -62,12 +85,13 @@ function ready() {
 
     };
 
-    var fly1 = new fly('flyer1');
-    fly1.init();
-
-    var fly2 = new fly('flyer2');
-    fly2.init();
-
+    var count = prompt();
+    if (count > 0 && count <= 5){
+        create(count);
+    } else {
+        alert('0 < count < 5');
+        var count = prompt();
+    }
 }
 
 
